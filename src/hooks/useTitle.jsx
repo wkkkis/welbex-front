c2868bb73d10797487ead06db0038a96fcb3201a
Issak <aizekyuy@yandex.ru>
2022-10-24 03:36:59 +0600

    some fixes

--- src/hooks/useTitle.jsx
+++ src/hooks/useTitle.jsx
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 
 export const usePageTitle = () => {
     const rename = useCallback((value) => {
-        document.title = `${value && value + " |"} TrustTrade`;
+        document.title = value;
     }, []);
 
     return {
